d88f3e70b8e14b4a20757ce2016e451bef14621a
Name <yousefelsobtasy15@gmail.com>
2024-10-18 11:03:17 +0300

    10

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
 import localFont from "next/font/local";
 import '@/app/global.css'
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
 
 export const metadata = {
   title: "100 qa",
