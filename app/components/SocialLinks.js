c31be6f0d34d308ee1712276bd5f83a6241a349d
Name <yousefelsobtasy15@gmail.com>
2024-10-24 19:29:22 +0300

    23-10-24-7_30

--- app/components/SocialLinks.js
+++ app/components/SocialLinks.js
@@ -1,15 +1,22 @@
-import { FaFacebookMessenger } from 'react-icons/fa'
-import { FaWhatsappSquare } from "react-icons/fa";
-import { SiGmail } from 'react-icons/si'
+import Link from 'next/link';
+import { FaFacebook, FaWhatsappSquare } from 'react-icons/fa';
+import { SiGmail } from 'react-icons/si';
 
 const SocialLinks = () => {
     return (
         <div className="socialLinks">
-            <span><FaFacebookMessenger /></span>
-            <span><FaWhatsappSquare /></span>
-            <span><SiGmail /></span>
+            <span>
+                <Link href="https://www.facebook.com/100021982488563" passHref>
+                    <FaFacebook />
+                </Link>
+            </span>
+            <span>
+                <Link href="https://wa.me/01009630771" passHref>
+                    <FaWhatsappSquare />
+                </Link>
+            </span>
         </div>
-    )
-}
+    );
+};
 
-export default SocialLinks
+export default SocialLinks;
