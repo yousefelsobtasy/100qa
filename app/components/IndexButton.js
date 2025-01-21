1bc004130ef58b1c5c49bfaf536be6ad66ff23b6
yousef <yousefelsobtasy15@gmail.com>
2025-01-21 20:13:07 +0200

    still trying to close mobile and tablet keyboard on submit (click enter)

--- app/components/IndexButton.js
+++ app/components/IndexButton.js
@@ -11,7 +11,7 @@ const IndexButton = ({ input, setInput, setCurrentQuestionIndex, setErrorMessage
                     if (e.key === 'Enter' && input > 0 && input <= qaLength - 1) {
                         setCurrentQuestionIndex(input - 1)
                         setErrorMessage(null);
-                        window.focus() // To make keyboard of mobile and tablets useres close on submit (click enter)
+                        e.target.blur() // To make keyboard of mobile and tablets useres close on submit (click enter)
                     } else if (e.key === 'Enter') {
                         setErrorMessage(`Please enter a number between 1 and ${qaLength}`);
                     }
